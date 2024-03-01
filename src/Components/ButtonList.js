cc97075df7d6b0b793add13d5d75b69875ba5aaf
Dipak Waghmare <dipakswaghmare555@gmail.com>
2024-03-01 23:28:49 +0530

    Created Button List Component

---
+++ src/Components/ButtonList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "./Button";
+
+const ButtonList = () => {
+    return (
+        <div className="flex">
+            <Button name={"All"} />
+            <Button name={"Live"} />
+            <Button name={"News"} />
+            <Button name={"Stock Market"} />
+            <Button name={"Sports"} />
+            <Button name={"Music"} />
+            <Button name={"Movies"} />
+            <Button name={"Cricket"} />
+            <Button name={"WWE"} />
+            <Button name={"Football"} />
+            <Button name={"Motivation"} />
+            <Button name={"Yoga"} />
+            <Button name={"News"} />
+            <Button name={"Update"} />
+            <Button name={"Watched"} />
+        </div>
+    );
+};
+
+export default ButtonList;
