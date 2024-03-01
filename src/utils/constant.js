6d89eee2712cdd1592d8c8489ef51df27d3f290c
Dipak Waghmare <dipakswaghmare555@gmail.com>
2024-03-01 23:40:11 +0530

    Create constant.js

---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyBn3FaUQfRCvgX4EGcOpPcl79GmsoB8fPs";
+
+export const YOUTUBE_VIDEOS_API =
+    "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+    [GOOGLE_API_KEY];
